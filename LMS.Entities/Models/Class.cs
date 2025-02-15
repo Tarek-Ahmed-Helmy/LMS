5139168219972e2a01ea145ef680bd280075d5a5
Mohamed Badr <mohamedbadrahmed@outlook.com>
2025-02-16 01:14:37 +0200

    add 3 models

---
+++ LMS.Entities/Models/Class.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Entities.Models;
+
+public class Class
+{
+    [Key]
+    public int ClassId { get; set; }
+
+    [Required]
+    public int GradeLevel { get; set; }
+
+    [Required]
+    public int Capacity { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string ClassNum { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
+
