e4d509dd4c2cd4573b92aaa99f75bf347d11972d
nadrmhmd057 <nadrmhmd057@gmail.com>
2025-03-02 21:25:31 +0200

    Group-4

---
+++ LMS.Entities/Models/Attendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Entities.Models;
+
+public class Attendance
+{
+    [Key]
+    public int AttendanceID { get; set; }
+
+    [ForeignKey("ApplicationUser")]
+    public int UserID { get; set; }
+    public DateTime? Date { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [RegularExpression("Present|Absent|Late", ErrorMessage = "Status must be 'Present', 'Absent', or 'Late'.")]
+    public string Status { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string? Remarks { get; set; }
+}
