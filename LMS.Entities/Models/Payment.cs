6f5f7ae3383e4b3d27bed2f36de713525d2ba55b
mohamed fathi <Mkhalifaali>
2025-03-02 22:25:20 +0200

    add-group5-models

---
+++ LMS.Entities/Models/Payment.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Entities.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int PaymentId { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; }
+
+        [Required, MaxLength(20)]
+        [EnumDataType(typeof(PaymentMethod))]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Required, MaxLength(20)]
+        [EnumDataType(typeof(PaymentStatus))]
+        public PaymentStatus PaymentStatus { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string ReceiptPath { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+
+        [Required]
+        public int ParentId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("ParentId")]
+        public virtual Parent Parent { get; set; }
+
+        [ForeignKey("StudentId")]
+        public virtual Student Student { get; set; }
+    }
+    public enum PaymentMethod
+    {
+        Cash,
+        Card,
+        BankTransfer
+    }
+
+    public enum PaymentStatus
+    {
+        Paid,
+        Pending,
+        Overdue
+    }
+}
---
+++ LMS.Entities/Models/Schedule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Entities.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int ScheduleId { get; set; }
+
+        [Required, MaxLength(20)]
+        [EnumDataType(typeof(DayOfWeek))]
+        public DayOfWeek DayOfWeek { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
+
+        public DateTime AssignedDate { get; set; }
+
+        [Required]
+        public int ClassId { get; set; }
+
+        [Required]
+        public int TeacherId { get; set; }
+
+        [Required]
+        public int SubjectId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("ClassId")]
+        public virtual Class? Class { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public virtual Teacher? Teacher { get; set; }
+
+        [ForeignKey("SubjectId")]
+        public virtual Subject? Subject { get; set; }
+    }
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
