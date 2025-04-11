f090f43af795c0e15a2e6735a3ec42c21be8250e
Mohamed Badr <mohamedbadrahmed@outlook.com>
2025-04-11 05:00:52 +0200

    add progress

---
+++ LMS.Client/ViewModels/Admin/ParentRegistrationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using LMS.Entities.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Web.ViewModels.AdminViewModels
+{
+    public class ParentRegistrationViewModel
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Occupation { get; set; }
+    }
+}
