c7505e77c6843d7cbe335c9e84f4cb1c45f12b66
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2025-04-11 14:05:42 +0200

    add all dashboards

---
+++ LMS.Client/Areas/Student/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using LMS.Entities.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Web.Areas.StudentArea.Controllers;
+
+[Area("Student")]
+public class DashboardController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public DashboardController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
