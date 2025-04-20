a02adb16e798d26acbc76852844e7e6ed32e0939
mohamed fathi <Mkhalifaali>
2025-04-20 23:09:20 +0200

    StudentManagementController

---
+++ LMS.Client/ViewModels/Teacher/TeacherStudentListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Web.ViewModels.Teacher;
+
+public class TeacherStudentListViewModel
+{
+    [Display(Name = "Student ID")]
+    public string StudentId { get; set; }
+
+    [Display(Name = "Full Name")]
+    public string FullName { get; set; }
+
+    [Display(Name = "Student Number")]
+    public string StudentNumber { get; set; }
+
+    [Display(Name = "Class")]
+    public string ClassName { get; set; }
+}
